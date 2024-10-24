e4d12aa6a126bf9214e47459880f4f2c54f75ef2
Atiq Ur Rehman <atiq.ur.rehman@kodewizardz.com>
2024-10-24 19:46:15 +0500

    Initial commit

---
+++ src/components/CesiumComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Color } from "cesium";
+import { Viewer, Entity } from "resium";
+
+export default function CesiumComponent() {
+  return (
+    <Viewer full />
+  )
+}
